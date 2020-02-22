6b2c27c39a80a6dfb67e9e90b03f86564315dcbf
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-02-22 20:43:36 +0100

    sa

--- Block Buster S/Assets/Scripts/Block.cs
+++ Block Buster S/Assets/Scripts/Block.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    [SerializeField] AudioClip breakSound;
+
+    // cached referance
+    Level level;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.CoutBreakableBlocks();
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         Destroy(gameObject);
     }
 }
